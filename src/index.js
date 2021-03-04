7d86cd080d1f75b9b16262016b3eedb54a6cbf89
Gholamrezahsz <gholamreza15402@gmail.com>
2021-03-04 21:26:11 +0330

    config redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./assets/css/all.css";
 import "./assets/css/font.css";
 import "./assets/css/style.css";
+import { store } from "./redux/people/store";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById("root")
 );
