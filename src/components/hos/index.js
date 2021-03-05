17de5347d248d2a76d07c8f148bb3efb5cd18091
afshin <af.afshin92@gmail.com>
2021-03-05 11:27:58 +0330

    import Header to hoc

--- src/components/hos/index.js
+++ src/components/hos/index.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
+import Header from "./header";
+
+
 
 const MainLayout = ({ children }) => {
   return (
     <Fragment>
+      <Header />
       <main>
         <div className="container">
           <div className="peoples">{children}</div>
