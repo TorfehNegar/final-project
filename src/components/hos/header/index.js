cdf05bd9c8b83ab9258911933bd28b8faf19a078
afshin <af.afshin92@gmail.com>
2021-03-05 11:27:04 +0330

    Header's files created & styled it

---
+++ src/components/hos/header/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+import React from 'react'
+import "./header.scss";
+const Header = () => {
+    return (
+        <div>
+            <div className="container">
+                ssdfdgfghj
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
