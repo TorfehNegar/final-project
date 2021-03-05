ce99acb0a7a2e865093f3c3e8e94e3088d95de29
afshin <af.afshin92@gmail.com>
2021-03-05 19:39:42 +0330

    Name section created and styled

---
+++ src/components/hos/header/Name/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Name.scss';
+
+function Name() {
+  return (
+    <div className="name">
+      <h1> myGroupName</h1>
+    </div>
+  );
+}
+
+export default Name;
