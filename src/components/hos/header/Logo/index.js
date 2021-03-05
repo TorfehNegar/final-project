25040457663151840a29973947d9d10c3dbe3686
afshin <af.afshin92@gmail.com>
2021-03-05 11:59:21 +0330

    logo component created

--- src/components/hos/header/Logo/index.js
+++ src/components/hos/header/Logo/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import Logo from '../../../../assets/img/logo.png';
 
+function TorfehNegarLogo() {
+  return (
+    <div>
+      <img className="logo" src={Logo}/>
+    </div>
+  );
+}
+
+export default TorfehNegarLogo;
