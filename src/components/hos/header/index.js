7addb3810806515099f4dda67c8cebab5e070456
afshin <af.afshin92@gmail.com>
2021-03-05 17:33:27 +0330

    navbar import to header

--- src/components/hos/header/index.js
+++ src/components/hos/header/index.js
@@ -2,13 +2,15 @@
 
 import React from 'react'
 import "./header.scss";
-import TorfehNegarLogo from './Logo/index'
+import TorfehNegarLogo from './Logo/index';
+import NavBar from './NavBar/index';
 
 const Header = () => {
     return (
         <div>
             <div className="Header__container">
                 <TorfehNegarLogo/>
+                <NavBar/>
             </div>
         </div>
     )
