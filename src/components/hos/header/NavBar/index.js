054426d6d56a709b023343f2dadbef983c3db36e
afshin <af.afshin92@gmail.com>
2021-03-05 17:33:06 +0330

    navbar created and styled

---
+++ src/components/hos/header/NavBar/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./NavBar.scss";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <nav className="navbar__nav">
+        <ul>
+          <li>
+            <a> home </a>
+          </li>
+          <li>
+            <a> home </a>
+          </li>
+          <li>
+            <a> home </a>
+          </li>
+          <li>
+            <a> home </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
