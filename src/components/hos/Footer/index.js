e49feaf12ada6adf5542610aef8d40db6f979b5b
afshin <af.afshin92@gmail.com>
2021-03-06 15:34:37 +0330

    footer's sections import in footer

---
+++ src/components/hos/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import socialMedia from './socialMedia/index';
+import Logo from './Logo/index';
+import Description from './Description/index';
+
+function Footer() {
+    return (
+        <div className="fooetr">
+            
+            
+        </div>
+    )
+}
+
+export default Footer;
