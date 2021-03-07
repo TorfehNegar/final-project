81de7e9cc8d08600cdd8a4d60a79ef79f00ca6a9
Arman Eskandari <arman_eskandari@live.com>
2021-03-07 23:00:31 +0330

    created CardActions component

---
+++ src/components/card/imageBox/hovered/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardActions = () => {
+  return (
+    <div>
+        add to / remove from favorites button and share button go here
+    </div>
+  );
+};
+
+export default CardActions;
