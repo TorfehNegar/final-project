2edd1ec59bd58d0dd84c2da7e2f50f04a8c7b042
Arman Eskandari <arman_eskandari@live.com>
2021-03-07 23:12:42 +0330

    added props and specified the proptypes

--- src/components/card/imageBox/hovered/index.js
+++ src/components/card/imageBox/hovered/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import FavoriteButton from "./favorite/FavoriteButton";
+import PropTypes from 'prop-types';
 
-const CardActions = () => {
+const CardActions = ({like, isFavorite, id}) => {
   return (
     <div>
-      <FavoriteButton />
+      <FavoriteButton like={like} isFavorite={isFavorite} id={id} />
       {/*share component goes here*/}
     </div>
   );
+};
+
+CardActions.propTypes = {
+  isFavorite: PropTypes.bool,
+  like: PropTypes.func,
+  id: PropTypes.number
 };
 
 export default CardActions;
