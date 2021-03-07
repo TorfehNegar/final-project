9e208cec5b623866564337393ce66a11e7a1d7f7
Arman Eskandari <arman_eskandari@live.com>
2021-03-07 22:25:33 +0330

    created CardContainer component

---
+++ src/components/card/CardContainer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CardContainer = () => {
+  return (
+    <div>
+      {/* use map method here */}
+      <div>
+        <div className="image">
+            image goes here
+        </div>
+        <div className="text">
+            people info goes here
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardContainer;
