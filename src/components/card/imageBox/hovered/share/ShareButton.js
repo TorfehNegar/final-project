392c08ade5255d32415e414cda966393b14f7484
Arman Eskandari <arman_eskandari@live.com>
2021-03-07 23:26:09 +0330

    created ShareButton component

---
+++ src/components/card/imageBox/hovered/share/ShareButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ShareButton = () => {
+  return (
+    <button style={{background: "none"}}>
+      <img
+        src="https://img.icons8.com/bubbles/50/4a90e2/share.png"
+        alt="share"
+      />
+    </button>
+  );
+};
+
+export default ShareButton;
