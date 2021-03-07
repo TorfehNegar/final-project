f1b9249e34883d89ca56f8e00a796b013775176a
Arman Eskandari <arman_eskandari@live.com>
2021-03-07 23:17:46 +0330

    added props and specified proptypes

--- src/components/card/imageBox/CardImage.js
+++ src/components/card/imageBox/CardImage.js
@@ -1,14 +1,21 @@
 import React from "react";
 import CardActions from "./hovered";
+import PropTypes from 'prop-types';
 
-const CardImage = () => {
+const CardImage = ({like, isFavorite, id}) => {
   return (
     <div>
       <img className="starwars-image" src="https://gamefa.com/wp-content/uploads/2019/12/lucasfilm-releases-a-cool-new-poster-for-star-wars-the-rise-of-skywalker-social.jpg"
         alt="star wars logo" />
-      <CardActions />
+      <CardActions isFavorite={isFavorite} like={like} id={id} />
     </div>
   );
+};
+
+CardImage.propTypes = {
+  like: PropTypes.func,
+  isFavorite: PropTypes.bool,
+  id: PropTypes.number
 };
 
 export default CardImage;
