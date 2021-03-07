e12f371e90c58360de32cbb53ac34b6e8f22a76d
Arman Eskandari <arman_eskandari@live.com>
2021-03-08 01:18:24 +0330

    added Link to people/

--- src/components/card/contentBox/index.js
+++ src/components/card/contentBox/index.js
@@ -1,18 +1,23 @@
+/* eslint-disable no-undef */
 import React from "react";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 
-const CardText = ({people}) => {
+const CardText = ({people, pageId}) => {
   return (
     <div className="card-text">
-      <h1>{people.name}</h1>
+      <Link to={{pathname:"/people/" + people.id, pageId: pageId}}>
+        <h1>{people.name}</h1>
+      </Link>
       <p>{people.gender}</p>
     </div>
   );
 };
 
 CardText.propTypes = {
-  people: PropTypes.object
+  people: PropTypes.object,
+  pageId: PropTypes.number
 };
 
 export default CardText;
