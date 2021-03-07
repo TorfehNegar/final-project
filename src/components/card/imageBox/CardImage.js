ea8c18d31d52ea98803f1f9e3fa537bb899d4e1e
Arman Eskandari <arman_eskandari@live.com>
2021-03-07 23:16:29 +0330

    imported CardActions into CardImage

--- src/components/card/imageBox/CardImage.js
+++ src/components/card/imageBox/CardImage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import CardActions from "./hovered";
 
 const CardImage = () => {
   return (
     <div>
       <img className="starwars-image" src="https://gamefa.com/wp-content/uploads/2019/12/lucasfilm-releases-a-cool-new-poster-for-star-wars-the-rise-of-skywalker-social.jpg"
         alt="star wars logo" />
+      <CardActions />
     </div>
   );
 };
