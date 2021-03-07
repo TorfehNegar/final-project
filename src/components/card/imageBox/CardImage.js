e231b80460fea9e07ea40e116c2c50b4bcc40cc4
Arman Eskandari <arman_eskandari@live.com>
2021-03-08 00:33:22 +0330

    styling CardImage

--- src/components/card/imageBox/CardImage.js
+++ src/components/card/imageBox/CardImage.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const CardImage = ({like, isFavorite, id}) => {
   return (
-    <div>
+    <div className="card-image flexy">
       <img className="starwars-image" src="https://gamefa.com/wp-content/uploads/2019/12/lucasfilm-releases-a-cool-new-poster-for-star-wars-the-rise-of-skywalker-social.jpg"
         alt="star wars logo" />
       <CardActions isFavorite={isFavorite} like={like} id={id} />
