f6c0e3417a75b2786c7335c302b6fa92bb926e6a
Arman Eskandari <arman_eskandari@live.com>
2021-03-08 12:07:41 +0330

    added people as props & changed the structure

--- src/components/card/CardContainer.js
+++ src/components/card/CardContainer.js
@@ -3,31 +3,26 @@
 import React from "react";
 import CardText from "./contentBox";
 import CardImage from "./imageBox/CardImage";
+import PropTypes from 'prop-types';
 import "./cardStyles.scss";
 
-
-/*
- **** requirements **** 
- 1. peoples array ( containing objects of star wars characters ) from store 
- 2. isFavorite property of each object
- 3. id of each object
- 4. a function to pass it to FavoriteButton component through props drilling -
-    to get the id and isFavorite property of the liked card
-*/
-
-const CardContainer = () => {
+const CardContainer = ({people}) => {
   return (
-    <div>
-      {/* use map method here */}
-      {peoples.length !==0 ?
-        peoples.map((people) => (
-          <div key={people.id} className="card-container">
-            <CardImage isFavorite={people.isFavorite} like={() => like(people.id,people.isFavorite)} id={people.id} /> {/* shows the picture of the people */}
-            <CardText people={people} /> {/* shows people's name and gender */}
-          </div>
-        )) : <h1>در حال بارگذاری...</h1>}
+    <div className="card-container">
+      <CardImage 
+        isFavorite={people.isFavorite} 
+        like={() => like(people.id,people.isFavorite)} 
+        id={people.id} 
+      />
+      <CardText 
+        people={people} 
+      />
     </div>
   );
+};
+
+CardContainer.propTypes = {
+  people: PropTypes.object
 };
 
 export default CardContainer;
