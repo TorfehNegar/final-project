3fc3d4c33a7e74a878ec8d9e434eabc2b1f946c5
afshin <af.afshin92@gmail.com>
2021-03-08 20:33:45 +0330

    copyright section updated

--- src/components/hos/Footer/copyright/index.js
+++ src/components/hos/Footer/copyright/index.js
@@ -4,8 +4,8 @@ import "./copyright.scss";
 const CopyRight = () => {
   return (
     <div className="footer__copy__right">
-      <p> All Rights Reserved.  <h4> TorfehNegar Develop Team </h4>&copy; Copyright 2021 </p>
-      <p> Designed by: TorfehNegar Develop Team</p>        
+      <span> All Rights Reserved.  <h4> TorfehNegar Develop Team </h4>&copy; Copyright 2021 </span>
+      <span> Designed by: TorfehNegar Develop Team</span>        
     </div>
   );
 };
