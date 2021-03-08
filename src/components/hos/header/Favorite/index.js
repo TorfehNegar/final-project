b0dfa729bb454ca20762a0b928e2fb19ec285774
farnaz khoshgoftar <farnaz.khoshgoftar@gmail.com>
2021-03-08 17:24:01 +0330

    create favorite counter in header

--- src/components/hos/header/Favorite/index.js
+++ src/components/hos/header/Favorite/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 // import { ReactComponent as Heart } from '../../../../assets/svg/heart.svg';
 import './Favorite.scss';
+import {useSelector} from 'react-redux';
 
 function Favorite() {
+  const peoples=useSelector(state=>state.peoples); 
+  let favoriteCounter=peoples.filter(item =>{
+    if(item.isFavorite==true){
+      return item;
+    }} 
+  );
   return (
     <div className="favorite">
-      <p> <button> <span className="favorite__box"> 2 </span> </button> </p>
+      <p> <button> <span className="favorite__box"> {favoriteCounter.length} </span> </button> </p>
     </div>
   );
 }
