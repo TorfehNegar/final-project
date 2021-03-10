7707737d17d4d4c43445c27525570535d4675c6a
afshin <af.afshin92@gmail.com>
2021-03-11 02:54:58 +0330

    footer improved

--- src/components/hos/Footer/copyright/index.js
+++ src/components/hos/Footer/copyright/index.js
@@ -4,8 +4,13 @@ import "./copyright.scss";
 const CopyRight = () => {
   return (
     <div className="footer__copy__right">
-      <span> All Rights Reserved.  <h4> TorfehNegar Develop Team </h4>&copy; Copyright 2021 </span>
-      <span> Designed by: TorfehNegar Develop Team</span>        
+      <div>
+        <span> All Rights Reserved.  <h4> TorfehNegar Develop Team </h4>&copy; Copyright 2021 </span>
+      </div>
+      <div>
+        <span> Designed by: TorfehNegar Develop Team</span> 
+      </div>
+             
     </div>
   );
 };
